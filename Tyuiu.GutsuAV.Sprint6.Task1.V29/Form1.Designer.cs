d9e6806322953c8197016b7ace0dfd9b15ac4f30
Tyuiu.GutsuAV@outlook.com <gutsu05@mail.ru>
2023-12-03 18:37:59 +0500

    Task1

---
+++ Tyuiu.GutsuAV.Sprint6.Task1.V29/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace Tyuiu.GutsuAV.Sprint6.Task1.V29
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxStart_GAV = new System.Windows.Forms.TextBox();
+            this.textBoxStop_GAV = new System.Windows.Forms.TextBox();
+            this.textBoxResult_GAV = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxStart_GAV
+            // 
+            this.textBoxStart_GAV.Location = new System.Drawing.Point(398, 144);
+            this.textBoxStart_GAV.Name = "textBoxStart_GAV";
+            this.textBoxStart_GAV.Size = new System.Drawing.Size(100, 20);
+            this.textBoxStart_GAV.TabIndex = 0;
+            // 
+            // textBoxStop_GAV
+            // 
+            this.textBoxStop_GAV.Location = new System.Drawing.Point(398, 198);
+            this.textBoxStop_GAV.Name = "textBoxStop_GAV";
+            this.textBoxStop_GAV.Size = new System.Drawing.Size(100, 20);
+            this.textBoxStop_GAV.TabIndex = 0;
+            // 
+            // textBoxResult_GAV
+            // 
+            this.textBoxResult_GAV.Location = new System.Drawing.Point(398, 255);
+            this.textBoxResult_GAV.Name = "textBoxResult_GAV";
+            this.textBoxResult_GAV.Size = new System.Drawing.Size(100, 20);
+            this.textBoxResult_GAV.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxResult_GAV);
+            this.Controls.Add(this.textBoxStop_GAV);
+            this.Controls.Add(this.textBoxStart_GAV);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Спринт 6 | Таск 1 | Вариант 29 | Гуцу А. В.";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxStart_GAV;
+        private System.Windows.Forms.TextBox textBoxStop_GAV;
+        private System.Windows.Forms.TextBox textBoxResult_GAV;
+    }
+}
+
---
+++ Tyuiu.GutsuAV.Sprint6.Task1.V29/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tyuiu.GutsuAV.Sprint6.Task1.V29.Lib;
+namespace Tyuiu.GutsuAV.Sprint6.Task1.V29
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataService ds = new DataService();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonDone_GAV_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                int startValue = Convert.ToInt32(textBoxStart_GAV.Text);
+                int stopValue = Convert.ToInt32(textBoxStop_GAV.Text);
+
+                string strLine;
+
+                int len = ds.GetMassFunction(startValue, stopValue).Length;
+
+                double[] valueArray;
+                valueArray = new double[len];
+
+                valueArray = ds.GetMassFunction(startValue, stopValue);
+                textBoxResult_GAV.Text = "";
+                textBoxResult_GAV.AppendText("---------+--------------" + Environment.NewLine);
+                textBoxResult_GAV.AppendText("|     X  |     f(x)    |" + Environment.NewLine);
+                textBoxResult_GAV.AppendText("---------+--------------" + Environment.NewLine);
+
+                for (int i = 0; i <= len - 1; i++)
+                {
+                    strLine = String.Format("|{0,5:d}   |    {1,6:f2}   |", startValue, valueArray[i]);
+                    textBoxResult_GAV.AppendText(strLine + Environment.NewLine);
+                    startValue++;
+                }
+                textBoxResult_GAV.AppendText("---------+--------------" + Environment.NewLine);
+            }
+            catch
+            {
+                MessageBox.Show("Введены неверные данные", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonInfo_GAV_Click(object sender, EventArgs e)
+        {
+
+            MessageBox.Show("Таск 1 выполнил студент группы ИИПБ-23-1 Гуцу Александр Владимирович", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
