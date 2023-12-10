0723f57109a86917d2d9f6a573c24ed7cf3da27d
Tyuiu.GutsuAV@outlook.com <gutsu05@mail.ru>
2023-12-10 21:01:05 +0500

    Task7

---
+++ Tyuiu.GutsuAV.Sprint6.Task7.V15/FormMain.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace Tyuiu.GutsuAV.Sprint6.Task7.V15
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.saveFileDialogMatrix_GAV = new System.Windows.Forms.OpenFileDialog();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.openFileDialogTask_GAV = new System.Windows.Forms.OpenFileDialog();
+            this.buttonOpenFile_GAV = new System.Windows.Forms.Button();
+            this.dataGridViewInput_GAV = new System.Windows.Forms.DataGridView();
+            this.dataGridViewOutPut_GAV = new System.Windows.Forms.DataGridView();
+            this.buttonDone_GAV = new System.Windows.Forms.Button();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.buttonSave_GAV = new System.Windows.Forms.Button();
+            this.buttonDone_PKR = new System.Windows.Forms.Button();
+            this.toolTipButtons_GAV = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonInfo_GAV = new System.Windows.Forms.Button();
+            this.panelLoadFromFile_GAV = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewInput_GAV)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOutPut_GAV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // saveFileDialogMatrix_GAV
+            // 
+            this.saveFileDialogMatrix_GAV.FileName = "openFileDialog1";
+            // 
+            // openFileDialogTask_GAV
+            // 
+            this.openFileDialogTask_GAV.FileName = "openFileDialog1";
+            // 
+            // buttonOpenFile_GAV
+            // 
+            this.buttonOpenFile_GAV.Location = new System.Drawing.Point(329, 80);
+            this.buttonOpenFile_GAV.Name = "buttonOpenFile_GAV";
+            this.buttonOpenFile_GAV.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpenFile_GAV.TabIndex = 0;
+            this.buttonOpenFile_GAV.Text = "button1";
+            this.buttonOpenFile_GAV.UseVisualStyleBackColor = true;
+            this.buttonOpenFile_GAV.Click += new System.EventHandler(this.buttonOpenFile_GAV_Click);
+            // 
+            // dataGridViewInput_GAV
+            // 
+            this.dataGridViewInput_GAV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewInput_GAV.Location = new System.Drawing.Point(352, 261);
+            this.dataGridViewInput_GAV.Name = "dataGridViewInput_GAV";
+            this.dataGridViewInput_GAV.Size = new System.Drawing.Size(240, 150);
+            this.dataGridViewInput_GAV.TabIndex = 1;
+            // 
+            // dataGridViewOutPut_GAV
+            // 
+            this.dataGridViewOutPut_GAV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewOutPut_GAV.Location = new System.Drawing.Point(306, 153);
+            this.dataGridViewOutPut_GAV.Name = "dataGridViewOutPut_GAV";
+            this.dataGridViewOutPut_GAV.Size = new System.Drawing.Size(240, 150);
+            this.dataGridViewOutPut_GAV.TabIndex = 2;
+            // 
+            // buttonDone_GAV
+            // 
+            this.buttonDone_GAV.Location = new System.Drawing.Point(440, 42);
+            this.buttonDone_GAV.Name = "buttonDone_GAV";
+            this.buttonDone_GAV.Size = new System.Drawing.Size(75, 23);
+            this.buttonDone_GAV.TabIndex = 3;
+            this.buttonDone_GAV.Text = "button1";
+            this.buttonDone_GAV.UseVisualStyleBackColor = true;
+            this.buttonDone_GAV.Click += new System.EventHandler(this.buttonDone_GAV_Click);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            // 
+            // buttonSave_GAV
+            // 
+            this.buttonSave_GAV.Location = new System.Drawing.Point(306, 13);
+            this.buttonSave_GAV.Name = "buttonSave_GAV";
+            this.buttonSave_GAV.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave_GAV.TabIndex = 4;
+            this.buttonSave_GAV.Text = "button1";
+            this.buttonSave_GAV.UseVisualStyleBackColor = true;
+            this.buttonSave_GAV.Click += new System.EventHandler(this.buttonSave_GAV_Click);
+            // 
+            // buttonDone_PKR
+            // 
+            this.buttonDone_PKR.Location = new System.Drawing.Point(306, 104);
+            this.buttonDone_PKR.Name = "buttonDone_PKR";
+            this.buttonDone_PKR.Size = new System.Drawing.Size(75, 23);
+            this.buttonDone_PKR.TabIndex = 5;
+            this.buttonDone_PKR.Text = "button1";
+            this.buttonDone_PKR.UseVisualStyleBackColor = true;
+            // 
+            // toolTipButtons_GAV
+            // 
+            this.toolTipButtons_GAV.Location = new System.Drawing.Point(669, 153);
+            this.toolTipButtons_GAV.Name = "toolTipButtons_GAV";
+            this.toolTipButtons_GAV.Size = new System.Drawing.Size(75, 23);
+            this.toolTipButtons_GAV.TabIndex = 6;
+            this.toolTipButtons_GAV.Text = "button1";
+            this.toolTipButtons_GAV.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(743, 204);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(8, 8);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // buttonInfo_GAV
+            // 
+            this.buttonInfo_GAV.Location = new System.Drawing.Point(610, 41);
+            this.buttonInfo_GAV.Name = "buttonInfo_GAV";
+            this.buttonInfo_GAV.Size = new System.Drawing.Size(75, 23);
+            this.buttonInfo_GAV.TabIndex = 8;
+            this.buttonInfo_GAV.Text = "button2";
+            this.buttonInfo_GAV.UseVisualStyleBackColor = true;
+            // 
+            // panelLoadFromFile_GAV
+            // 
+            this.panelLoadFromFile_GAV.Location = new System.Drawing.Point(112, 2);
+            this.panelLoadFromFile_GAV.Name = "panelLoadFromFile_GAV";
+            this.panelLoadFromFile_GAV.Size = new System.Drawing.Size(200, 100);
+            this.panelLoadFromFile_GAV.TabIndex = 9;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelLoadFromFile_GAV);
+            this.Controls.Add(this.buttonInfo_GAV);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.toolTipButtons_GAV);
+            this.Controls.Add(this.buttonDone_PKR);
+            this.Controls.Add(this.buttonSave_GAV);
+            this.Controls.Add(this.buttonDone_GAV);
+            this.Controls.Add(this.dataGridViewOutPut_GAV);
+            this.Controls.Add(this.dataGridViewInput_GAV);
+            this.Controls.Add(this.buttonOpenFile_GAV);
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.Text = "Спринт 6 | Вариант 15 | Таск 7 | Гуцу А. В.";
+            this.Load += new System.EventHandler(this.FormMain_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewInput_GAV)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOutPut_GAV)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog saveFileDialogMatrix_GAV;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.OpenFileDialog openFileDialogTask_GAV;
+        private System.Windows.Forms.Button buttonOpenFile_GAV;
+        private System.Windows.Forms.DataGridView dataGridViewInput_GAV;
+        private System.Windows.Forms.DataGridView dataGridViewOutPut_GAV;
+        private System.Windows.Forms.Button buttonDone_GAV;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.Button buttonSave_GAV;
+        private System.Windows.Forms.Button buttonDone_PKR;
+        private System.Windows.Forms.Button toolTipButtons_GAV;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonInfo_GAV;
+        private System.Windows.Forms.Panel panelLoadFromFile_GAV;
+    }
+}
+
---
+++ Tyuiu.GutsuAV.Sprint6.Task7.V15/FormMain.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tyuiu.GutsuAV.Sprint6.Task7.V15.Lib;
+using System.IO;
+namespace Tyuiu.GutsuAV.Sprint6.Task7.V15
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+            openFileDialogTask_GAV.Filter = "Значения, разделенные запятыми(*.csv)|*.csv|Всефайлы(*.*)|*.*";
+            saveFileDialogMatrix_GAV.Filter = "Значения, разделенные запятыми(*.csv)|*.csv|Всефайлы(*.*)|*.*";
+        }
+        static string openFile;
+        static int rows;
+        static int columns;
+        DataService ds = new DataService();
+        public int[,] LoadFromData(string path)
+        {
+            string file = File.ReadAllText(path);
+            file = file.Replace('\n', '\r');
+            string[] lines = file.Split(new char[] { '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            rows = lines.Length;
+            columns = lines[0].Split(';').Length;
+
+            int[,] array = new int[rows, columns];
+
+            for (int i = 0; i < rows; i++)
+            {
+                string[] line_mas = lines[i].Split(';');
+                for (int j = 0; j < columns; j++)
+                {
+                    array[i, j] = Convert.ToInt32(line_mas[j]);
+                }
+            }
+            return array;
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonOpenFile_GAV_Click(object sender, EventArgs e)
+        {
+
+            openFileDialogTask_GAV.ShowDialog();
+            openFile = openFileDialogTask_GAV.FileName;
+
+            int[,] arrayValues = new int[rows, columns];
+            arrayValues = LoadFromData(openFile);
+
+            dataGridViewInput_GAV.RowCount = rows;
+            dataGridViewInput_GAV.ColumnCount = columns;
+            dataGridViewOutPut_GAV.RowCount = rows;
+            dataGridViewOutPut_GAV.ColumnCount = columns;
+
+            for (int i = 0; i < columns; i++)
+            {
+                dataGridViewInput_GAV.Columns[i].Width = 50;
+                dataGridViewOutPut_GAV.Columns[i].Width = 50;
+            }
+
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < columns; c++)
+                {
+                    dataGridViewInput_GAV.Rows[r].Cells[c].Value = arrayValues[r, c];
+                }
+            }
+            arrayValues = ds.GetMatrix(openFile);
+            buttonDone_GAV.Enabled = true;
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void buttonSave_GAV_Click(object sender, EventArgs e)
+        {
+            saveFileDialogMatrix_GAV.FileName = "OutPutFileTask7.csv";
+            saveFileDialogMatrix_GAV.InitialDirectory = Directory.GetCurrentDirectory();
+            saveFileDialogMatrix_GAV.ShowDialog();
+
+            string path = saveFileDialogMatrix_GAV.FileName;
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool exists = fileInfo.Exists;
+            if (exists)
+            {
+                File.Delete(path);
+            }
+
+            int rows = dataGridViewOutPut_GAV.RowCount;
+            int columns = dataGridViewOutPut_GAV.ColumnCount;
+            string str = "";
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (j != columns - 1)
+                    {
+                        str += dataGridViewOutPut_GAV.Rows[i].Cells[j].Value + ";";
+                    }
+                    else
+                    {
+                        str += dataGridViewInput_GAV.Rows[i].Cells[j].Value;
+                    }
+                }
+                File.AppendAllText(path, str + Environment.NewLine);
+                str = "";
+            }
+        }
+
+        private void buttonDone_GAV_Click(object sender, EventArgs e)
+        {
+            int[,] arrayValues = new int[rows, columns];
+            arrayValues = ds.GetMatrix(openFile);
+
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < columns; c++)
+                {
+                    dataGridViewOutPut_GAV.Rows[r].Cells[c].Value = arrayValues[r, c];
+                }
+            }
+            buttonSave_GAV.Enabled = true;
+        }
+        private void buttonOpenFile_PKR_MouseEnter(object sender, EventArgs e)
+        {
+
+        }
+        private void buttonDone_PKR_MouseEnter(object sender, EventArgs e)
+        {
+
+        }
+        private void buttonSave_PKR_MouseEnter(object sender, EventArgs e)
+        {
+
+        }
+        private void buttonInfo_PKR_MouseEnter(object sender, EventArgs e)
+        {
+
+        }
+        private void buttonInfo_PKR_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void buttonOpenFile_PKR_MouseMove(object sender, MouseEventArgs e)
+        {
+            buttonOpenFile_GAV.Cursor = System.Windows.Forms.Cursors.Hand;
+
+        }
+        private void buttonDone_PKR_MouseMove(object sender, MouseEventArgs e)
+        {
+            buttonDone_GAV.Cursor = System.Windows.Forms.Cursors.Hand;
+
+        }
+        private void buttonSave_PKR_MouseMove(object sender, MouseEventArgs e)
+        {
+            buttonSave_GAV.Cursor = System.Windows.Forms.Cursors.Hand;
+
+        }
+        private void buttonInfo_PKR_MouseMove(object sender, MouseEventArgs e)
+        {
+            buttonInfo_GAV.Cursor = System.Windows.Forms.Cursors.Hand;
+
+        }
+
+        private void FormMain_Load_1(object sender, EventArgs e)
+        {
+            dataGridViewInput_GAV.RowCount = 50;
+            dataGridViewOutPut_GAV.RowCount = 50;
+
+            dataGridViewInput_GAV.ColumnCount = 50;
+            dataGridViewOutPut_GAV.ColumnCount = 50;
+
+            panelLoadFromFile_GAV.Width = this.Width / 2;
+
+            for (int i = 0; i < 50; i++)
+            {
+                dataGridViewInput_GAV.Columns[i].Width = 25;
+                dataGridViewOutPut_GAV.Columns[i].Width = 25;
+            }
+        }
+    }
+}
